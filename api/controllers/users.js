fa4f0786cd027f06fba79c6fe4d6bfa271aafdea
Karan Palan <karanpalan007@gmail.com>
2024-07-07 22:03:57 +0530

    feat(api): implement logic for CRUD of user and video

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -17,6 +17,7 @@ export const update = async (req, res, next) => {
         return next(createError(403, "Please update your account"))
     }
 }
+
 export const deleteUser = async (req, res, next) => {
     if (req.params.id === req.user.id) {
         try {
@@ -30,18 +31,56 @@ export const deleteUser = async (req, res, next) => {
         return next(createError(403, "Please update your account"))
     }
 }
+
 export const getUser = async (req, res, next) => {
-
+    try {
+        const user = await User.findById(req.params.id)
+        res.status(200).json(user)
+    } catch (err) {
+        next(err)
+    }
 }
+
 export const subscriber = async (req, res, next) => {
-
+    try {
+        await User.findById(req.user.id, {
+            $push:{subscribedUsers:req.params.id}
+        })
+        await User.findByIdAndUpdate(req.params.id, {
+            $inc:{subscribers:1}
+        })
+        res.status(200).json("You are now subscribed!")
+    } catch (err) {
+        next(err)
+    }
 }
+
 export const unsubscribe = async (req, res, next) => {
-
+    try {
+        await User.findById(req.user.id, {
+            $pull:{subscribedUsers:req.params.id}
+        })
+        await User.findByIdAndUpdate(req.params.id, {
+            $inc:{subscribers:-1}
+        })
+        res.status(200).json("You are now un-subscribed!")
+    } catch (err) {
+        next(err)
+    }
 }
+
 export const like = async (req, res, next) => {
+    try {
 
+    } catch (err) {
+        next(err)
+    }
 }
-export const dislike = async (req, res, next) => {
 
+export const dislike = async (req, res, next) => {
+    try {
+
+    } catch (err) {
+        next(err)
+    }
 }
--- api/routes/video.js
+++ api/routes/video.js
@@ -1,8 +1,23 @@
 import express from "express"
-import { } from "../controllers/video.js";
+import { addVideo, addView, random, sub, trend } from "../controllers/video.js";
+import { verifyToken } from "../verifyToken.js";
 
 const router = express.Router();
 
+router.post("/", verifyToken, addVideo)
 
+router.post("/:id", verifyToken, addVideo)
+
+router.post("/:id", verifyToken, addVideo)
+
+router.post("/find/:id", addVideo)
+
+router.put("/view/:id", addView)
+
+router.put("/trend", trend)
+
+router.put("/random", random)
+
+router.put("/sub", verifyToken, sub)
 
 export default router; 
